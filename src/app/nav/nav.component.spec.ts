975eace8b908a4a64d140c3ea7f306410df71f9f
PABLO JUNQUERA BARRO <0014209@insags.com>
2024-03-20 16:39:25 +0100

    unit test services and routes

---
+++ src/app/nav/nav.component.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NavComponent } from "./nav.component";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { RouterTestingModule } from "@angular/router/testing";
+import { Router } from "@angular/router";
+
+class ComponentTestRoute {}
+
+describe('Nav compnent', ()=>{
+
+    let component: NavComponent;
+    let fixture: ComponentFixture<NavComponent>
+
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes([
+                    {path: 'home', component: ComponentTestRoute },
+                    {path: 'home', component: ComponentTestRoute }
+            ])
+            ],
+            declarations: [
+                NavComponent
+            ],
+            providers:[],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+        }).compileComponents();
+    });
+
+    beforeEach(()=> {
+        fixture = TestBed.createComponent(NavComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should navigate', () => {
+        const router = TestBed.inject(Router);
+        const spy1 = spyOn(router,'navigate');
+        component.navTo('home');
+        expect(spy1).toHaveBeenCalledWith(['/home']);
+        component.navTo('cart');
+        expect(spy1).toHaveBeenCalledWith(['/cart']);
+    });
+
+});
--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from "@angular/core/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { Book } from "../models/book.model";
 import { environment } from "src/environments/environment.prod";
+import Swal from "sweetalert2";
 
 
 const listBook: Book[] = [
@@ -30,6 +31,16 @@ const listBook: Book[] = [
     }
 ]; 
 
+const book: Book = 
+    {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 8,
+        amount: 7
+    }
+;
+
 describe('BookService', () => {
     let service: BookService;
     let httpMock: HttpTestingController;
@@ -51,8 +62,12 @@ describe('BookService', () => {
         service = TestBed.inject(BookService);
         httpMock = TestBed.inject(HttpTestingController);
 
+        storage = [];
         spyOn(localStorage, 'getItem').and.callFake((key:string)=> {
             return storage[key] ? storage[key]: null;
+        });
+        spyOn(localStorage, 'setItem').and.callFake((key: string, value: string)=>{
+            return storage[key] = value;
         });
     });
 
@@ -80,4 +95,27 @@ describe('BookService', () => {
 
     });
 
+    it('addBookToCart add a book succesfully when the list does not exists ', () => {
+        
+        const toast = {
+            fire: () => null
+        } as any;
+        const spy1 = spyOn(Swal, 'mixin').and.callFake(()=>{
+            return toast;
+        });
+        let listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(0);
+        service.addBookToCart(book);
+        listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(1);
+        expect(spy1).toHaveBeenCalled();
+    });
+    it('removeBooksFromCart removes the list from the localStorage', () => {
+        service.addBookToCart(book);
+        let listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(1);
+        service.removeBooksFromCart();
+        listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(0);
+    });
 });
